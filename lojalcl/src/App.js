1bf7dd9d0fc80cdc05cd98d596a45fef799a55b1
rianrp <riansatro@gmail.com>
2023-01-05 16:18:55 -0300

    dd

--- lojalcl/src/App.js
+++ lojalcl/src/App.js
@@ -15,7 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          testando
+          testa
+          
         </a>
       </header>
     </div>
